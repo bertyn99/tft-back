e9c6f5fa364c18d9335af602a1456a33114248ac
Bertyn Boulikou <bboulikou@gmail.com>
2021-11-03 13:49:34 +0100

    ajout du common route

--- lib/config/app.ts
+++ lib/config/app.ts
@@ -1,13 +1,16 @@
 import express, {Request,Response,Application} from 'express';
 import bodyParser from "body-parser";
 import { ItemsRoutes } from "../routes/items";
+import {CommonRoutes} from "../routes/common_routes"
 class App {
    public app: express.Application;
    private test_routes: ItemsRoutes = new ItemsRoutes(); 
+   private common: CommonRoutes= new CommonRoutes();
    constructor() {
       this.app = express();
       this.config();
       this.test_routes.route(this.app); 
+      this.common.route(this.app)
    }
    private config(): void {
       // support application/json type post data
