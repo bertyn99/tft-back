dfe8a21cb5f06f2cdb67aa9964444eed7002e19a
Bertyn Boulikou <bboulikou@gmail.com>
2021-11-03 14:02:19 +0100

    ajout des service et model commun

--- lib/config/app.ts
+++ lib/config/app.ts
@@ -1,14 +1,21 @@
 import express, {Request,Response,Application} from 'express';
 import bodyParser from "body-parser";
+import * as mongoose from 'mongoose';
+import environment from "../environment";
 import { ItemsRoutes } from "../routes/items";
 import {CommonRoutes} from "../routes/common_routes"
 class App {
    public app: express.Application;
+   public mongoUrl: string = 'mongodb://localhost/' + environment.getDBName();
+
+   //route
    private test_routes: ItemsRoutes = new ItemsRoutes(); 
    private common: CommonRoutes= new CommonRoutes();
+
    constructor() {
       this.app = express();
       this.config();
+      this.mongoSetup();
       this.test_routes.route(this.app); 
       this.common.route(this.app)
    }
@@ -18,5 +25,10 @@ class App {
       //support application/x-www-form-urlencoded post data
       this.app.use(express.urlencoded({ extended: false }));
    }
+
+   private mongoSetup(): void {
+      mongoose.connect(this.mongoUrl);
+   }
 }
+
 export default new App().app;
