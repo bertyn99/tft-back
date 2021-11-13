1696f8acd7659401164e26e94c856b259e052b73
Bertyn Boulikou <bboulikou@gmail.com>
2021-11-13 01:45:23 +0100

    fix model item

--- lib/modules/item/model.ts
+++ lib/modules/item/model.ts
@@ -1,27 +1,28 @@
 import { ModificationNote } from "../common/model";
 
+export interface Effects {
+    health?: Number,
+    armor?: Number,
+    as?: Number,
+    ap?: Number,
+    ad?: Number,
+    damage?: Number,
+    bonusAd?: Number,
+    magicResist?: Number,
+    CritChance?: Number,
+    mana?: Number,
+    bonusAs?: Number,
+    magicReduction?: Number,
+
+}
 export interface IItem {
     _id?: String;
     name: String;
-    desc:String;
-    effect:{
-         health?: Number,
-         armor?:Number,
-         as?:Number,
-         ap?:Number,
-         ad?:Number,
-         damage?:Number,
-         bonusAd?:Number,
-         magicResist?:Number,
-         CritChance?:Number,
-         mana?:Number,
-         bonusAs?:Number,
-         magicReduction?:Number,
+    desc: String;
+    effect: Effects,
+    icon: String;
+    unique: Boolean;
+    from: Array<Number>;
 
-   },
-   icon:String;
-   unique:Boolean;
-   from:Array<Number>;
-   
-    modification_notes: ModificationNote[]
+    modification_notes?: ModificationNote[]
 }
